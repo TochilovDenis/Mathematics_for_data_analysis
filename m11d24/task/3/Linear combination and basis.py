2b4784cc46a78cd3d79c05d8a6620876c96bc83c
TochilovDenis <denis-tochilov@yandex.ru>
2025-11-26 19:13:41 +0300

    Линейная комбинация и базис Ввод input.txt Вывод стандартный вывод
    Вам даны три вектора v1,v2,v3, такие что:
    vi​∈Z2.
    
    Ваша задача - написать программу, которая вычислит, возможно ли выразить вектор v3 как линейную комбинацию векторов v1,v2, и, если да, то найдет коэффициенты λ1,λ2 такие, что v3 = λ1v1 + λ2v2 и λi ∈ Z.
    
    Формат входных данных
     * Три строки, каждая из которых задает соответсвтующий вектор.
     * В каждой строке координаты вектора записаны через пробел.
    
    Ограничения
    ⦁	Координаты векторов и коэффициенты λi представлены целыми числами.
    ⦁	Любая из координат векторов по модулю не превосходит 10 2.
    ⦁	λi по модулю не превосходит 10.
    
    Формат выходных данных
    Если вектор v3 является линейной комбинацией векторов v1,v2, то выведите коэффициенты λ1, λ2. Иначе, выведите строку NO_SOLUTION.
    
    Пример 1
    
    input.txt
    2 3
    4 -1
    16 3
    
    Вывод
    2 3
    
    Пример 2
    
    input.txt
    5 3
    1 2
    -1 8
    
    Вывод
    NO_SOLUTION

--- m11d24/task/3/Linear combination and basis.py
+++ m11d24/task/3/Linear combination and basis.py
@@ -1,34 +1,25 @@
+import numpy as np
+
 def main():
     with open('input.txt', 'r') as f:
-        v1 = list(map(int, f.readline().strip().split()))
-        v2 = list(map(int, f.readline().strip().split()))
-        v3 = list(map(int, f.readline().strip().split()))
+        v1 = np.array(list(map(int, f.readline().strip().split())))
+        v2 = np.array(list(map(int, f.readline().strip().split())))
+        v3 = np.array(list(map(int, f.readline().strip().split())))
     
-    n = len(v1)
-    
-    # Инициализируем коэффициенты значениями, которые заведомо не в диапазоне
     result_lambda1 = -11
     result_lambda2 = -11
     
     for lambda1 in range(-10, 11):
         for lambda2 in range(-10, 11):
-            mas = [0] * n
+            # Вычисляем линейную комбинацию с помощью NumPy
+            mas = lambda1 * v1 + lambda2 * v2
             
-            vectors = [v1, v2]
-            lambdas = [lambda1, lambda2]
-            
-            for i in range(2):
-                current_vector = vectors[i]
-                current_lambda = lambdas[i]
-                for y in range(n):
-                    mas[y] += current_vector[y] * current_lambda
-            
-            if mas == v3:
+            # Сравниваем массивы с использованием np.array_equal
+            if np.array_equal(mas, v3):
                 result_lambda1 = lambda1
                 result_lambda2 = lambda2
                 break
         
-        # Проверяем, нашли ли решение во внутреннем цикле
         if result_lambda1 != -11:
             break
     
