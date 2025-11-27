772f2b8bc3deb8971ac4b2aee51e9b97ea1c4cae
TochilovDenis <denis-tochilov@yandex.ru>
2025-11-27 19:04:57 +0300

    Реализация умножения матриц вручную
    Ввод
    input.txt
    Вывод
    стандартный вывод
    Вам даны две матрицы A и B, такие что:
    A∈M m×n(R), B∈M h×k(R).
    
    Ваша задача - написать программу, которая вычислит матрицу C=AB, если данная операция возможна. При этом нужно обойтись без использования встроенных методов numpy для умножения матриц, а реализовать его самостоятельно.
    
    Формат входных данных
    В первой строке даны два целых числа m и n, где:
    m — число строк матрицы A (2≤m≤10).
    n — число столбцов матрицы A (2≤n≤10).
    Далее следуют m строк по n чисел в каждой, задающих матрицу A.
    В(m+1) строке даны два целых числа h и k, где:
    h — число строк матрицы B (2≤h≤10).
    k — число столбцов матрицы B (2≤k≤10).
    Далее следуют h строк по k чисел в каждой, задающих матрицу B.
    
    Формат выходных данных
    Выведите матрицу
    C∈Mm×k(R). Если матрицы A и B не согласованы по размерам, то выведите NOT_DEFINED.
    
    Пример 1
    
    Ввод
    2 2
    1 0
    0 1
    2 3
    1 0 3
    0 1 0
    
    Вывод
    1 0 3
    0 1 0
    
    Пример 2
    Ввод
    2 2
    1 1
    0 1
    3 2
    1 0
    0 1
    3 0
    
    Вывод
    NOT_DEFINED
    
    Пример 3
    Ввод
    2 2
    1 3
    2 1
    2 2
    1 0
    0 3
    
    Вывод
    1 9
    2 3

---
+++ m11d26/B/Implementing matrix multiplication manually.py
@@ -0,0 +1,36 @@
+def main():
+    m, n = map(int, input().split())
+    mtr1 = []
+    for i in range(m):
+        mtr1.append(list(map(int, input().split())))
+
+    h, k = map(int, input().split())
+    mtr2 = []
+    for i in range(h):
+        mtr2.append(list(map(int, input().split())))
+
+    if n != h:
+        print("NOT_DEFINED")
+        return
+
+    # mtr3 = [[0 for _ in range(k)] for _ in range(m)]
+    mtr3 = []
+    for i in range(m):      
+        row = []           
+        for j in range(k):
+            row.append(0)
+        mtr3.append(row)
+
+    for i in range(m):
+        for j in range(k):
+             for l in range(n):
+                mtr3[i][j] += mtr1[i][l] * mtr2[l][j]
+
+    for i in range(m):
+        for j in range(k):
+            print(mtr3[i][j], end=" ")
+        print()
+
+
+if __name__ == '__main__':
+    main()
